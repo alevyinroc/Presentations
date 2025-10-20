fa86f7304a970344749bb320da05884122cd8d9a
Andy Levy <andy@flxsql.com>
2025-10-19 22:41:57 -0400

    Make table names safer

--- Answering the Auditor's Call/Reporting.sql
+++ Answering the Auditor's Call/Reporting.sql
@@ -7,17 +7,18 @@ set @PreviousCollection =  (select top 1 CollectionDate from (
 select CollectionDate from Auditor.Patchlevels where CollectionDate < @LatestCollection) as PL order by CollectionDate desc);
 
 
-declare @TableName sysname = 'DatabaseRoleMembers';
+--declare @TableName sysname = N'DatabaseRoleMembers';
+declare @TableName sysname = N'AllPermissions';
 
 declare @ColumnList nvarchar(max);
 select @ColumnList = string_agg(quotename(c.[name]), ',') from sys.columns as C join sys.tables as T on C.object_id = T.object_id join sys.schemas as S on S.schema_id = T.schema_id
-where S.[name] = 'Auditor' and T.[name] = @TableNAme and c.name <> 'CollectionDate';
+where S.[name] = N'Auditor' and T.[name] = @TableName and c.name <> N'CollectionDate';
 
 declare @Sql nvarchar(max);
 
-set @Sql = N'select ' + @ColumnList + N' from [Auditor].' + @TableName + N' where CollectionDate = @DateParam1
+set @Sql = N'select ' + @ColumnList + N' from [Auditor].' + quotename(@TableName) + N' where CollectionDate = @DateParam1
 EXCEPT
-select ' + @ColumnList + N' from [Auditor].' + @TableName + N' where CollectionDate = @DateParam2;'
+select ' + @ColumnList + N' from [Auditor].' + quotename(@TableName) + N' where CollectionDate = @DateParam2;'
 
 select 'Added between ' + cast(@PreviousCollection as char(10)) + ' and ' + cast(@LatestCollection as char(10)) as [Reviewing];
 exec sp_executesql @sql, N'@DateParam1 date,@DateParam2 date', @LatestCollection,@PreviousCollection;
